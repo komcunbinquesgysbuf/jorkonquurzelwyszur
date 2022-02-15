13ceeeff08f1b46d2ce09253fadfcd5c659eb7e8
bardikinkespo <bardikinkespo@commodea.com>
2022-02-15 13:17:26 +0000

    Use different strategies for build end develop

--- gatsby-node.js
+++ gatsby-node.js
@@ -1,3 +1,22 @@
+const {readdir, readFile, stat, writeFile} = require('fs/promises');
+const findMarkdownFiles = path => readdir(path)
+    .then(files => Promise.all(files.map(f => [path, f].join('/')).map(p => stat(p)
+        .then(s => s.isFile() && p.match(/\.md$/) ? [p] : (s.isDirectory() ? findMarkdownFiles(p) : []))
+    )))
+    .then(arrays => arrays.flat())
+;
+const replaceAbsolutePathsinMarkdownFiles = basePath => findMarkdownFiles(basePath).then(files => Promise.all(
+    files.map(file => readFile(file, {encoding: 'UTF-8'}).then(data =>
+            data.match(/\/ckzmnri0c000ipj862t51ivd1\//) && writeFile(
+                file,
+                data.replace(
+                    /\/ckzmnri0c000ipj862t51ivd1\//g,
+                    '../'.repeat(file.replace(basePath, '.').split('/').length - 1) + 'images/'
+                )
+            )
+    ))
+));
+
 exports.createPages = async ({ actions }) => {
   const { createPage } = actions
   createPage({
@@ -7,3 +26,6 @@ exports.createPages = async ({ actions }) => {
     defer: true,
   })
 };
+if (!process.env.DEV) {
+    exports.onPreBootstrap = () => replaceAbsolutePathsinMarkdownFiles(process.cwd() + '/src/pages');
+}
