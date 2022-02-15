310e69d3a5f8e25380cec547dcdfb5f414c622c1
komcunbinquesgysbuf <komcunbinquesgysbuf@commodea.com>
2022-02-15 17:07:04 +0000

    Apply changes from Gatsby Auth Tutorial...
    https://www.gatsbyjs.com/tutorial/authentication-tutorial/

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,31 +1,24 @@
-import * as React from "react"
+import React from "react"
 import { Link } from "gatsby"
-import { StaticImage } from "gatsby-plugin-image"
-
+import { getUser, isLoggedIn } from "../services/auth"
 import Layout from "../components/layout"
-import Seo from "../components/seo"
-
-const IndexPage = () => (
-  <Layout>
-    <Seo title="Home" />
-    <h1>Hi people</h1>
-    <p>Welcome to your new Gatsby site.</p>
-    <p>Now go build something great.</p>
-    <StaticImage
-      src="../images/gatsby-astronaut.png"
-      width={300}
-      quality={95}
-      formats={["auto", "webp", "avif"]}
-      alt="A Gatsby astronaut"
-      style={{ marginBottom: `1.45rem` }}
-    />
-    <p>
-      <Link to="/page-2/">Go to page 2</Link> <br />
-      <Link to="/using-typescript/">Go to "Using TypeScript"</Link> <br />
-      <Link to="/using-ssr">Go to "Using SSR"</Link> <br />
-      <Link to="/using-dsg">Go to "Using DSG"</Link>
-    </p>
-  </Layout>
-)
-
-export default IndexPage
+export default function Home() {
+    return (
+        <Layout>
+            <h1>Hello {isLoggedIn() ? getUser().name : "world"}!</h1>
+            <p>
+                {isLoggedIn() ? (
+                    <>
+                        You are logged in, so check your{" "}
+                        <Link to="/app/profile">profile</Link>
+                    </>
+                ) : (
+                    <>
+                        You should <Link to="/app/login">log in</Link> to see restricted
+                        content
+                    </>
+                )}
+            </p>
+        </Layout>
+    )
+}
