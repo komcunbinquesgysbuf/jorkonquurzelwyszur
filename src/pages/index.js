1a281817a6c81ca9f62ae55f2bd73adcd9e93944
komcunbinquesgysbuf <komcunbinquesgysbuf@commodea.com>
2022-02-28 12:43:22 +0000

    Redirect to start page defined in nav.yml

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,7 +1,17 @@
 import React from "react"
-import {navigate} from "gatsby"
+import {graphql, navigate, useStaticQuery} from "gatsby"
 
-export default function Home() {
-    typeof window !== 'undefined' && navigate("/main");
+const Home = () => {
+    const {file: {childListsYaml: {file: {relativeDirectory, name}}}} = useStaticQuery(graphql`
+        {
+            file(relativePath: {eq: "nav.yml"}) {
+                childListsYaml {
+                    file { relativeDirectory name }
+                }
+            }
+        }
+    `);
+    typeof window !== 'undefined' && navigate('/' + [relativeDirectory, name].filter(p => !!p).join('/'));
     return null;
-}
+};
+export default Home;
